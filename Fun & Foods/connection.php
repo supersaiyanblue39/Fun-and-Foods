3da7f18a452b9c8ad5123574a1c8056bbc70ab4f
gaur <rishabhkhanna39@live.com>
2017-11-06 23:56:54 +0530

    final

---
+++ Fun & Foods/connection.php
@@ -0,0 +1,11 @@
+<?php
+//mysql_connect('localhost','root','gaur');
+//mysql_select_db("fun_foods");
+
+$servername="localhost";
+$user="root";
+$pwd="gaur";
+$dbname="fun_foods";
+$con = new mysqli($servername,$user,$pwd,$dbname);
+
+?>
